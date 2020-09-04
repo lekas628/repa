55fffbcf2465584a6084e4fa84f8f26fb215cae6
leo <leka628@yandex.ru>
2020-09-04 12:06:34 +0300

    1

--- repa_1/repa_1/Program.cs
+++ repa_1/repa_1/Program.cs
@@ -6,7 +6,25 @@ namespace repa_1
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      //Console.WriteLine("Input ur name: ");
+      //string name = Console.ReadLine();
+      //Console.WriteLine($"Hello {name}");
+      //Console.ReadKey();
+
+      object text_1 = "Hello"; // хранит любого типа
+      
+      var text_2 = "Welcome to the club"; // неявная типизация, сразу иниц
+      // Console.WriteLine(text_2.GetType().ToString()); 
+
+      string text_3 = ":)";
+
+      Console.WriteLine(text_1);
+      {
+        Console.WriteLine(text_2);
+        Console.WriteLine($"okey {text_3}");
+      }
+
+      Console.ReadKey();
     }
   }
 }
