85600c7d61e8bbd6a127dc2b33765f2351552b6a
lekas628 <lekas628@gmail.com>
2020-09-04 10:06:51 +0300

    267

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -24,6 +24,7 @@ namespace ConsoleApp1
       Console.WriteLine("Hello World!");
       Console.WriteLine("Hello World!");
       Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello World!");
     }
   }
 }
