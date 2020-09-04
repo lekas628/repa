cc4f54942b896f6d0f443624d4b5be20efae8e83
lekas628 <lekas628@gmail.com>
2020-09-04 10:52:53 +0300

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,11 +4,13 @@ namespace ConsoleApp1
 {
   struct Person
   {
+    public enum Genders : int { Male, Female };
+
+
     public string firstName;
     public string lastName;
     public int age;
-
-    public enum Genders : int { Male, Female};
+    public Genders gender;
 
     public Person(string _firstName, string _lastName, int _age)
     {
