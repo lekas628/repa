453b6266a77507ec552bc67e7e4e897201c7ae5b
Леонид Плисов <lekas628@gmail.com>
2020-10-24 17:25:54 +0300

    .

--- изучение_лекций/изучение_лекций/Program.cs
+++ изучение_лекций/изучение_лекций/Program.cs
@@ -1,20 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace изучение_лекций
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int x = 10;
-            int z;
-            Sum(x, 15, out z);
-            Console.WriteLine(z);
+            Person person = new Person("Joe", 21, "my_random");
+            Console.WriteLine("object created");
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream(@"D:\people.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, person);
+                Console.WriteLine("объект сериализован");
+            }
+
+            using (FileStream fs = new FileStream(@"D:\people.dat", FileMode.OpenOrCreate))
+            {
+                Person newPerson = (Person)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newPerson.Name}, Возраст {newPerson.Year}");
+            }
         }
-        
-        static void Sum(int x, int y, out int a)
+    }
+
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+        [NonSerialized]
+        public string accNumber;
+
+        public Person(string _name, int _year, string _acc)
         {
-            a = x + y;
+            Name = _name;
+            Year = _year;
+            accNumber = _acc;
         }
     }
 }
