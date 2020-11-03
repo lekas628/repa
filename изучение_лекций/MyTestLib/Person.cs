cc38b5f22559a5184652086c1ce7441787e1f62e
Леонид Плисов <lekas628@gmail.com>
2020-11-03 18:40:27 +0300

    1

---
+++ изучение_лекций/MyTestLib/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MyTestLib
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+    }
+}
--- изучение_лекций/изучение_лекций/Program.cs
+++ изучение_лекций/изучение_лекций/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace изучение_лекций
 {
@@ -8,39 +8,21 @@ namespace изучение_лекций
     {
         static void Main()
         {
-            Person person = new Person("Joe", 21, "my_random");
-            Console.WriteLine("object created");
-            BinaryFormatter formatter = new BinaryFormatter();
-
-            using (FileStream fs = new FileStream(@"D:\people.dat", FileMode.OpenOrCreate))
+            ObservableCollection<User> users = new ObservableCollection<User>
             {
-                formatter.Serialize(fs, person);
-                Console.WriteLine("объект сериализован");
-            }
+                new User { Name = "Bill"},
+                new User { Name = "Tom" },
+                new User { Name = "Alice"}
+            };
 
-            using (FileStream fs = new FileStream(@"D:\people.dat", FileMode.OpenOrCreate))
-            {
-                Person newPerson = (Person)formatter.Deserialize(fs);
-                Console.WriteLine("Объект десериализован");
-                Console.WriteLine($"Имя: {newPerson.Name}, Возраст {newPerson.Year}");
-            }
         }
     }
 
-    [Serializable]
-    class Person
+
+
+
+    class User
     {
         public string Name { get; set; }
-        public int Year { get; set; }
-
-        [NonSerialized]
-        public string accNumber;
-
-        public Person(string _name, int _year, string _acc)
-        {
-            Name = _name;
-            Year = _year;
-            accNumber = _acc;
-        }
     }
 }
